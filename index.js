df85edacc44f81f0b0cb7e5fcab1ef2270849af0
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2017-05-05 23:17:51 -0500

    fix: relative path to message.txt

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var isNpmNotYarn = require('is-npm-not-yarn')
 var fs = require('fs')
+var path = require('path')
 
 function useYarn(message) {
   if (isNpmNotYarn) {
-    message = message || fs.readFileSync('message.txt', 'utf8')
+    message = message || fs.readFileSync(path.resolve(__dirname, 'message.txt'), 'utf8')
     console.error(message)
     process.exit(1)
   }
