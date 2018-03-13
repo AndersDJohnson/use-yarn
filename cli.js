1e38e682bcf1bd5fb3b52eb4b7954ed5313fbe47
Andreas Lubbe <git@lubbe.org>
2018-03-13 15:07:29 +0100

    Add support for -m and -f cli flags

--- cli.js
+++ cli.js
@@ -2,8 +2,20 @@
 
 var fs = require('fs')
 var path = require('path')
+var argv = require('minimist')(process.argv.slice(2));
 var useYarn = require('.')
 
-var message = fs.readFileSync(path.resolve(__dirname, 'message.txt'), 'utf8')
+var message
+if (argv.f) {
+  message = fs.readFileSync(argv.f, 'utf8')
+}
+
+if (!message && argv.m) {
+  message = argv.m
+}
+
+if (!message) {
+  message = fs.readFileSync(path.resolve(__dirname, 'message.txt'), 'utf8')
+}
 
 useYarn(message)
