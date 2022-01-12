e7b83d82717b37b0340d78c6968fcdc2dbe78fd3
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2022-01-12 06:36:16 -0600

    fix: support npx again with stdin

--- cli.js
+++ cli.js
@@ -1,15 +1,33 @@
 #!/usr/bin/env node
 
-var fs = require('fs')
-var path = require('path')
-var argv = require('minimist')(process.argv.slice(2));
-var useYarn = require('.')
+var fs = require("fs");
+var path = require("path");
+var argv = require("minimist")(process.argv.slice(2));
+var useYarn = require(".");
 
-var message
+var message;
 if (argv.f) {
-  message = fs.readFileSync(argv.f, 'utf8')
+  message = fs.readFileSync(argv.f, "utf8");
 } else if (argv.m) {
-  message = argv.m
+  message = argv.m;
 }
 
-useYarn(message)
+if (process.stdin.isTTY) {
+  useYarn({ message });
+} else {
+  process.stdin.on("readable", () => {
+    let buffer = "";
+
+    let chunk;
+
+    while ((chunk = process.stdin.read()) != null) {
+      buffer += chunk;
+    }
+
+    if (buffer) {
+      useYarn({ message, npm_execpath: buffer });
+    } else {
+      useYarn({ message });
+    }
+  });
+}
